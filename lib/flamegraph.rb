f51b563671b81c72f35c7544f60c105e7e66b414
Sam <sam.saffron@gmail.com>
2013-09-03 13:04:34 +1000

    clean up api a bit, erase a method call since we are taking a hard dependency

--- lib/flamegraph.rb
+++ lib/flamegraph.rb
@@ -3,24 +3,23 @@ require "fast_stack"
 require "flamegraph/version"
 require "flamegraph/sampler"
 require "flamegraph/renderer"
-require "flamegraph/fast_stack_sampler"
 
 module Flamegraph
   def self.generate(filename=nil, opts = {})
     fidelity = opts[:fidelity]  || 0.5
 
-    restuls = FastStack.profile(fidelity) do
+    backtraces = FastStack.profile(fidelity) do
       yield
     end
 
-    renderer = Flamegraph::Renderer.new(results)
-    result = renderer.graph_html
+    renderer = Flamegraph::Renderer.new(backtraces)
+    rendered = renderer.graph_html
 
     if filename
       File.open(filename,"w") do |f|
-        f.write(result)
+        f.write(rendered)
       end
     end
-    result
+    rendered
   end
 end
